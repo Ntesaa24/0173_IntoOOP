df94ef661842d1145c0e9d4bf2e272060af5bc10
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-04-23 10:13:09 +0700

    void output

--- Into_object.opp/Into_object.opp.cpp
+++ Into_object.opp/Into_object.opp.cpp
@@ -29,4 +29,11 @@ public:
 		cout << "SKS: ";
 		cin >> sks;
 	}
+
+	void output(){
+		cout << "Kode MK: " << kodeMK << endl;
+		cout << "Nama MK: " << namaMK << endl;
+		cout << "SKS: " << sks << endl;
+	}
 }; 
+
